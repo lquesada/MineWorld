722343f1242111ebb901ceea86d08b2a85e05223
Luis Quesada <luis@elezeta.com>
2013-06-08 19:04:10 +0200

    Created build file and fixed library path

--- src/com/elezeta/mineworld/MineWorldLauncher.java
+++ src/com/elezeta/mineworld/MineWorldLauncher.java
@@ -3,6 +3,8 @@
  */
 package com.elezeta.mineworld;
 
+import java.lang.reflect.Field;
+
 /**
  *
  * @author elezeta
@@ -10,6 +12,15 @@ package com.elezeta.mineworld;
 public class MineWorldLauncher {
  
     public static void main(String[] args) {
+    	System.setProperty("java.library.path", "MineWorld_lib/:"+System.getProperty("java.library.path"));
+    	Field fieldSysPath;
+		try {
+			fieldSysPath = ClassLoader.class.getDeclaredField( "sys_paths" );
+	    	fieldSysPath.setAccessible( true );
+	    	fieldSysPath.set( null, null );
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
         (new MineWorld()).run();
     }   
     
